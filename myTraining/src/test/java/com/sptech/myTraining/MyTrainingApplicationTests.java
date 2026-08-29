cc65e9a69b80f4469134e3e9e382c569456001fc
Guilherme-Mesquita06 <guilherme.mesquita@sptech.school>
2026-08-29 13:28:54 -0300

    Create: Inicializando dependencias do projeto SpringBoot

---
+++ myTraining/src/test/java/com/sptech/myTraining/MyTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptech.myTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
